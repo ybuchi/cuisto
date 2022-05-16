2902af01e5a200ca8761cd0314c0ba76a0a9d199
ybuchi <yabuch_ch@hotmail.com>
2022-05-16 19:02:57 -0400

    Configured basic routes.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Login from './Components/Login/Login';
+import Landing from './Components/LandingPage/LandingPage';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Cuisto</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Landing/>}/>
+      <Route path="login" element={<Login/>}/>
+    </Routes>
   );
 }
 
