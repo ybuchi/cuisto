b21c765ca892f5d76494a3a93cc6dcc2503b6bd7
ybuchi <yabuch_ch@hotmail.com>
2022-05-16 20:49:44 -0400

    Created basic login page

--- client/src/Components/Login/Login.js
+++ client/src/Components/Login/Login.js
@@ -1,8 +1,25 @@
 import React from "react";
+import "./Login.css";
+import Form from 'react-bootstrap/Form';
 
 function Login(){
     return(
-        <h1>Login</h1>
+        <menu>
+            <header>
+                <h1>Login</h1>
+            </header>
+            <Form>
+                <Form.Group>
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control type="text"></Form.Control>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password"></Form.Control>
+                </Form.Group>
+                
+            </Form>
+        </menu>
     )
 }
 
