322e6e0b36573a3fb37eb7345d8790093f0beb27
ybuchi <yabuch_ch@hotmail.com>
2022-05-25 11:58:03 -0400

    Can now activate and inactivate pantries in the nav bar.

--- client/src/Components/CustomHooks/useFetchUserPantries.js
+++ client/src/Components/CustomHooks/useFetchUserPantries.js
@@ -1,14 +1,18 @@
-import { useEffect, useState } from "react";
+import { useEffect, useContext } from "react";
+import { UserContext } from "../Contexts/UserContext"
 
 function useFetchUserPantries(){
-    const [userPantries, setUserPantries] = useState([])
+    const { userPantries, setUserPantries } = useContext(UserContext)
 
     useEffect(()=>{
         fetch("/user-pantries")
         .then(res => res.json())
-        .then(pantryData => setUserPantries(pantryData))
-    },[])
+        .then(pantryData => {
+            setUserPantries(pantryData)
+        })
+    },[setUserPantries])
 
+    console.log(userPantries);
     return [userPantries, setUserPantries]
 }
 export default useFetchUserPantries;
--- client/src/Components/NavBar/NavBar.js
+++ client/src/Components/NavBar/NavBar.js
@@ -7,22 +7,97 @@ import logo from "../Images/Logos/4.png";
 import logo2 from "../Images/Logos/6.png";
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Dropdown from 'react-bootstrap/Dropdown';
+import SplitButton from 'react-bootstrap/SplitButton';
 import Container from 'react-bootstrap/Container';
 import Offcanvas from 'react-bootstrap/Offcanvas';
-import useFetchActivePantryIngredients from "../CustomHooks/useFetchActivePantryIngredients";
+import useFetchUserPantries from "../CustomHooks/useFetchUserPantries";
 
 function NavBar(){
     useLoginState();
-    const [activePantries, setActivePantries ] = useFetchActivePantryIngredients();
-    console.log(activePantries);
+    const [userPantries, setUserPantries ] = useFetchUserPantries();
+    console.log(userPantries);
     const { isLoggedIn, setIsLoggedIn } = useContext(UserContext);
     const navigate = useNavigate();
+
+    function handleInactivatePantry(e){
+      const pantry_id = e.target.name
+      const configObj = {
+        method : "PATCH",
+        headers : {
+            "Content-Type" : "application/json",
+            "Accepts" : "application/json"
+        },
+        body: JSON.stringify({
+            active : false
+        })
+    }
+      fetch(`/user_pantries/${pantry_id}/activate`, configObj)
+        .then(res => res.json())
+        .then(updatedPantry => {
+            console.log("Updated Pantry", updatedPantry)
+            setUserPantries(userPantries.map(pantryObject => {
+                if(updatedPantry.id === pantryObject.id){
+                    return updatedPantry
+                }else{
+                    return pantryObject
+                }
+            }))
+        })
+    }
+
+    function handleActivatePantry(e){
+      const pantry_id = e.target.name
+      const configObj = {
+        method : "PATCH",
+        headers : {
+            "Content-Type" : "application/json",
+            "Accepts" : "application/json"
+        },
+        body: JSON.stringify({
+            active : true
+        })
+    }
+      fetch(`/user_pantries/${pantry_id}/activate`, configObj)
+        .then(res => res.json())
+        .then(updatedPantry => {
+            console.log("Updated Pantry", updatedPantry)
+            setUserPantries(userPantries.map(pantryObject => {
+                if(updatedPantry.id === pantryObject.id){
+                    return updatedPantry
+                }else{
+                    return pantryObject
+                }
+            }))
+        })
+    }
+
+    const mappedActivePantries = userPantries.filter(pantryObject => pantryObject.user_pantries[0].active).map(activePantryObj => {
+      return(
+        <Dropdown.Item key={activePantryObj.id} name={`${activePantryObj.id}`} onClick={handleInactivatePantry}>
+          {activePantryObj.pantry_name}
+        </Dropdown.Item>
+      )
+    })
+    const mappedInactivePantries = userPantries.filter(pantryObj => !pantryObj.user_pantries[0].active).map(inactivePantryObj=>{
+      return(
+        <Dropdown.Item key={inactivePantryObj.id} name={`${inactivePantryObj.id}`} onClick={handleActivatePantry}>
+          {inactivePantryObj.pantry_name}
+        </Dropdown.Item>
+      )
+    })
 
     const navLinks = isLoggedIn ? <Nav className="justify-content-end flex-grow-1 pe-3">
                                     <Nav.Link href="/dashboard">Dashboard</Nav.Link>
                                     <Nav.Link href="/recipe-library">Recipy Library</Nav.Link>
-                                    <Nav.Link href="/pantries">Pantries</Nav.Link>
                                     <Nav.Link href="/clubs">Clubs</Nav.Link>
+                                    <SplitButton variant="secondary" title="Pantries" onClick={()=> navigate("/pantries")}>
+                                      <Dropdown.Header>Active</Dropdown.Header>
+                                      {mappedActivePantries}
+                                      <Dropdown.Divider/>
+                                      <Dropdown.Header>Inactive</Dropdown.Header>
+                                      {mappedInactivePantries}
+                                    </SplitButton>
                                     <Nav.Link href="/profile">Profile</Nav.Link>
                                     <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
                                   </Nav> : <Nav className="justify-content-end flex-grow-1 pe-3">
