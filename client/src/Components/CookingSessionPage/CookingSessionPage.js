c2857c2e6a7ceb5f078379634bdba76a4deca49b
ybuchi <yabuch_ch@hotmail.com>
2022-06-01 14:07:21 -0400

    Can now select a pantry to automatically calculate which ingredients to remove. Calculations don't work yet.

--- client/src/Components/CookingSessionPage/CookingSessionPage.js
+++ client/src/Components/CookingSessionPage/CookingSessionPage.js
@@ -7,11 +7,26 @@ import Carousel from 'react-bootstrap/Carousel';
 import fabricBackground from "../Images/Backgrounds/Fabric.jpg"
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/esm/Container";
+import useFetchUserPantries from "../CustomHooks/useFetchUserPantries";
+import Modal from "react-bootstrap/Modal";
 
 function CookingSessionPage(){
     let { recipe_id } = useParams();
     const [recipeData, setRecipeData] = useFetchRecipeData(recipe_id);
     const [recipeIngredients, setRecipeIngredients] = useFetchRecipeIngredients(recipe_id)
+
+    const[showActivePantrySelection, setShowActivePantrySelection] = useState(false);
+    const[selectedPantry, setSelectedPantry] = useState("");
+
+    const [userPantries, setUserPantries ] = useFetchUserPantries();
+    const activePantries = userPantries.filter(pantryObject => pantryObject.user_pantries[0].active)
+
+    function selectPantry(e){
+        const pantryName = e.target.textContent
+        console.log(pantryName)
+        setSelectedPantry(pantryName)
+    }
+
     const [slideIndex, setSlideIndex] = useState(0)
     const navigate = useNavigate();
 
@@ -24,6 +39,16 @@ function CookingSessionPage(){
         setSlideIndex(parseInt(e.target.id))
     }
 
+    function handleFinishedCooking(){
+        //Ask the user to select a pantry to calculate which items to remove
+        setShowActivePantrySelection(true);
+
+
+        //Make a fetch call to retrieve the pantry ingredients with their amounts.
+
+        //In the .then, compare the pantry ingredients to recipe Ingredients and make calculations
+    }
+
     const mappedCarouselSteps = recipeData.steps ? recipeData.steps.map((step, index) => {
 
         return(
@@ -32,7 +57,7 @@ function CookingSessionPage(){
                 <Carousel.Caption className="top">
                     <h4>Step {index + 1} </h4>
                     <p className="recipe-step">{recipeData.steps[index]}</p>
-                    {index === (recipeData.steps.length -1) ? <Button>Finished Cooking!</Button> : null}
+                    {index === (recipeData.steps.length -1) ? <Button onClick={handleFinishedCooking}>Finished Cooking!</Button> : null}
                 </Carousel.Caption>
             </Carousel.Item>
         )
@@ -65,10 +90,28 @@ function CookingSessionPage(){
             {mappedCarouselSteps}
         </Carousel>
         {mappedListSteps}
-        </>
-        
 
-        
+        <Modal show={showActivePantrySelection} onHide={()=>setShowActivePantrySelection(false)}>
+            <Modal.Header>
+                <h3>Select a Pantry to Remove Ingredients From</h3>
+            </Modal.Header>
+            <Modal.Body>
+                {activePantries.map((pantryObject, index) => {
+                    return(
+                        <div key={index}>
+                            <p className="pantry-select" onClick={selectPantry}>{pantryObject.pantry_name}</p>
+                        </div>
+                    )
+                })}
+            </Modal.Body>
+            <Modal.Footer>
+                <p>Selected Pantry: <strong>{selectedPantry}</strong></p>
+                <Button>Remove Cooked Ingredients from Pantry</Button>
+                <Button>Manage Pantry Manually</Button>
+                <Button variant="secondary">Continue With No Action</Button>
+            </Modal.Footer>
+        </Modal>
+        </>
     )
 }
 
